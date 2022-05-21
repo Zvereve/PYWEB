6731221a0780bc49e4b6f6cadd5dbe68583e4448
ZverevE <dr.jekkill@yandex.ru>
2022-05-21 11:50:29 +0300

    init

--- login/views.py
+++ login/views.py
@@ -1,11 +1,11 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views import View
 
 
-class IndexView(View):
+class LoginView(View):
    def get(self, request):
        return render(request, 'login/index.html')
 
    def post(self, request):
-       return JsonResponse(request.POST, json_dumps_params={'indent': 4})
+       return HttpResponse(f'<h1>Hello, World</h1>')
